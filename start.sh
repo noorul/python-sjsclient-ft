22c89f5f718f745806612419263ad5af0686bc3b
Noorul Islam K M <noorul@noorul.com>
2017-02-24 12:16:23 +0530

    Remove start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SJS_JAR_PATH=$(find `pwd` -name spark-job-server.jar)
-echo SJS_JAR_PATH=$SJS_JAR_PATH
-exec $SPARK_HOME/bin/spark-submit --class spark.jobserver.JobServer ${SJS_JAR_PATH}
